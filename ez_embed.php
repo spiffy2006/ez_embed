8010228f3a26d4614dbe14c30a28f513f1f4a263
spiffy2006 <jcox@boostability.com>
2014-10-16 07:42:51 -0600

    Updated from current working version for more updated portfolio.

--- ez_embed.php
+++ ez_embed.php
@@ -1,78 +1,163 @@
 <?php
+
 /**
  * Plugin Name: Easy Code Embed
  * Description: This plugin directly injects raw code into a shortcode.
- * Version: 1.0
- * Author: Hanky Panky
+ * Version: 1.0.3
+ * Author: Jake Cox
  */
 
-
-/** Step 2 (from text above). */
-add_action( 'admin_menu', 'my_plugin_menu' );
+add_action( 'admin_menu', 'ez_embed_menu' );
 
 /** Step 1. */
-function my_plugin_menu() {
-	add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+function ez_embed_menu() {
+	add_menu_page( 'Easy Code Embed', 'EZ Code Embed', 'manage_options', 'easy_code_embed', 'easy_code_embed', 'dashicons-schedule', '100.3' );
 }
 
 /** Step 3. */
-function my_plugin_options() {
+function easy_code_embed() {
 	if ( !current_user_can( 'manage_options' ) )  {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
-  ?>
-  <script type="text/javascript">
-  window.onload = function() {
-   document.getElementById('easy-embed-title').addEventListener('keyup', updateTitle);
+	?>
+    <script type="text/javascript">
+		window.onload = function() {
+			document.getElementById('easy-embed-title').addEventListener('keyup', updateTitle);
 
-   function updateTitle() {
-       var d = document;
-       var ezTitle = d.getElementById('easy-embed-title').value;
-       var titleArray = ezTitle.split(' ');
-       var postIndex = titleArray.join('-');
-       d.getElementById('post_index').value = postIndex;
-       d.getElementById('shortcode_display').textContent = '[ez-embed title="' + postIndex + '"]';
-   }
-  }
-  </script>
-  <?php
+			function updateTitle() {
+				var d = document;
+				var ezTitle = d.getElementById('easy-embed-title').value;
+				var titleArray = ezTitle.split(' ');
+				var postIndex = titleArray.join('_');
+				d.getElementById('post_index').value = postIndex;
+				d.getElementById('shortcode_display').textContent = '[ez_embed title="' + postIndex + '"]';
+			}
+		} 
+    </script> 
+	<?php
+    $shortcodes = maybe_unserialize( get_option('easy_code_embed') );
+    echo '<h1>Your Shortcodes</h1>';
+    $scHTML = '<div id="shortcode_html">';
+    foreach ($shortcodes as $key => $sc_array) {
+        $scHTML .= '<div style="display: inline-block; padding: 8px 15px; box-shadow: 1px 1px 7px #333; background: #fff; margin-right: 12px; border-radius: 5px;"><h4 style="text-align: center;">[ez_embed title="' . $key . '"]' . '</h4>
+        <a style="display: inline-block; padding: 5px 10px;" href="javascript:void()" class="edit_link" data-index="' . $key . '">Edit</a>
+        <a style="display: inline-block; padding: 5px 10px;" href="javascript:void()" class="delete_link" data-index="' . $key . '">Delete</a></div>';
+    }
+    $scHTML .= '</div>';
+    echo $scHTML;
 	echo '<div class="wrap">';
-  echo '<div id="shortcode_display"></div>';
-	echo '<form id="easy-embed" method="post" action="schema.php">
-          <p><label for="easy-embed-title">Title your shortcode:</label></p>
-          <p><input id="easy-embed-title" type="text" class="wide-fat" name="title"></p>
-          <p><label for="easy-embed-text">Write your code here:</label></p>
-          <p><textarea id="easy-embed-text" class="wide-fat" name="content" cols="50" rows="100"></textarea></p>
-          <input type="submit" value="submit" />
-          <input id="post_index" type="hidden" name="post_index" />
-        </form>';
+	echo '<div id="shortcode_display"></div>';
+	echo '<form id="easy-embed" method="post">
+			<p><label for="easy-embed-title">Title your shortcode:</label></p>
+			<p><input id="easy-embed-title" type="text" class="wide-fat" name="title" /></p>
+			<p><label for="easy-embed-text">Write your code here:</label></p>
+			<p><textarea id="easy-embed-text" class="wide-fat" name="content" cols="100" rows="20"></textarea></p>			
+			<input id="post_index" type="hidden" name="post_index" />
+		  </form>';
+    echo '<button id="submit">Submit</button>';
+    echo '<div id="response"></div>';
 	echo '</div>';
 }
 
-if ( isset($_POST['post_index']) && isset($_POST['title']) && isset($_POST['content']) ) {
-  if (get_option('easy_code_embed')) {
-    $code_embed = maybe_unserialize( get_option('easy_code_embed') );
-    $code_embed[$_POST['post_index']]['title'] = $_POST['title'];
-    $code_embed[$_POST['post_index']]['content'] = $_POST['content'];
-    update_option('easy_code_embed', $code_embed);
-  } else {
-    $new_code_embed = array(
-      $_POST['post_index'] => array(
-        'title' => $_POST['title'],
-        'content' => $_POST['content']
-      ),
-    );
-    update_option('easy_code_embed', $new_code_embed);
-  }
-}
+add_action( 'admin_footer', 'ez_embed_js' );
 
-function ez_embed_shortcode() {
-    $a = shortcode_atts( array(
-        'foo' => 'something',
-        'bar' => 'something else',
-    ), $atts );
-
-    return "foo = {$a['foo']}";
-}
-add_shortcode( 'bartag', 'ez_embed_shortcode' );
+function ez_embed_js() { 
+$ajax_nonce = wp_create_nonce( "ez_embed_nonce" );
 ?>
+    <script type="text/javascript">
+        jQuery(document).ready(function($){
+            $('#submit').on('click', function() {
+                var data = {
+                    'action': 'ez_embed_update',
+                    'security': '<?php echo $ajax_nonce; ?>',
+                    'title': $('#easy-embed-title').val(),
+                    'content': $('#easy-embed-text').val(),
+                    'post_index': $('#post_index').val()
+                };
+                $.post(ajaxurl, data, function (response) {
+                    $('#response').html(response);
+                });
+                setTimeout(function(){location.reload()}, 1000);
+            });
+            $('.edit_link').on('click', function() {
+                var data = {
+                    'action': 'ez_embed_get',
+                    'security': '<?php echo $ajax_nonce; ?>',
+                    'shortcode': $(this).attr('data-index')
+                };
+                $.post(ajaxurl, data, function (response) {
+                    var returnArray = JSON.parse(response);
+                    for (var i in returnArray) {
+                       $('#' + i).val(returnArray[i]);
+                    }
+                });
+            });
+            $('.delete_link').on('click', function() {
+                var data = {
+                    'action': 'ez_embed_delete',
+                    'security': '<?php echo $ajax_nonce; ?>',
+                    'shortcode': $(this).attr('data-index')
+                };
+                $.post(ajaxurl, data, function (response) {
+                    $('#response').html(response);
+                });
+                $(this).parent().remove();
+            });
+        });
+    </script>
+<?php }
+    
+add_action('wp_ajax_ez_embed_update', 'ez_embed_callback');
+    
+function ez_embed_callback() {
+    check_ajax_referer( 'ez_embed_nonce', 'security' );
+    if (get_option('easy_code_embed')) {
+        $code_embed = maybe_unserialize( get_option('easy_code_embed') );
+        $code_embed[$_POST['post_index']]['title'] = $_POST['title'];
+        $code_embed[$_POST['post_index']]['content'] = $_POST['content'];
+        update_option('easy_code_embed', $code_embed);
+    } else {
+        $new_easy_code_embed = array(
+            $_POST['post_index'] => array (
+            'title' => $_POST['title'],
+            'content' => $_POST['content'],
+            'post_index' => $_POST['post_index']
+            ),
+        );
+        update_option('easy_code_embed', $new_easy_code_embed);
+    }
+    echo '<h2>Your shortcode was saved!</h2>';
+    die();
+}
+
+add_action('wp_ajax_ez_embed_get', 'ez_embed_get_callback');
+    
+function ez_embed_get_callback() {
+    check_ajax_referer( 'ez_embed_nonce', 'security' );
+    $content = maybe_unserialize( get_option('easy_code_embed') );
+    $return_array = array(
+        'easy-embed-title' => $content[$_POST['shortcode']]['title'],
+        'easy-embed-text' => stripslashes($content[$_POST['shortcode']]['content']),
+        'post_index' => $_POST['shortcode']
+    );
+    echo json_encode($return_array);
+    die();
+}
+
+add_action('wp_ajax_ez_embed_delete', 'ez_embed_delete_callback');
+    
+function ez_embed_delete_callback() {
+    check_ajax_referer( 'ez_embed_nonce', 'security' );
+    $content = maybe_unserialize( get_option('easy_code_embed') );    
+    unset($content[$_POST['shortcode']]);
+    update_option('easy_code_embed', $content);
+    echo '<h2>Your shortcode was removed!</h2>';
+    die();
+}
+    
+add_shortcode( 'ez_embed', 'ez_embed_shortcode' );
+
+function ez_embed_shortcode($atts) {
+    $ez_embed = maybe_unserialize( get_option('easy_code_embed') );
+    return stripslashes($ez_embed[$atts['title']]['content']);
+}
